3402844134f7129462c7e9e114e2b500d9ba19f1
Wojciech Fiszer <fiszer.wojciech@gmail.com>
2021-06-01 22:22:34 +0200

    Backup for old .zshrc file

--- apply.sh
+++ apply.sh
@@ -2,4 +2,12 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+backup () {
+    file=$1
+    if [ -f $file ]; then
+        cp $file $file"_backup_$(date +'%Y_%m_%d_%H_%M_%S')"
+    fi
+}
+
+backup ~/.zshrc
 cp $SCRIPT_DIR/zsh/.zshrc ~/
